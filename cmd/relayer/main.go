68c0f1afa69bccce55b0e07f215dae51e1c462ef
Sina Mahmoodi <itz.s1na@gmail.com>
2018-09-11 22:14:27 +0200

    Add geth as dependency, get acc balance for testing
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- cmd/relayer/main.go
+++ cmd/relayer/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 
+	common "github.com/ethereum/go-ethereum/common"
+	ethclient "github.com/ethereum/go-ethereum/ethclient"
 	ipfs "github.com/ipfs/go-ipfs-api"
 )
 
@@ -19,4 +22,18 @@ func main() {
 	}
 
 	log.Printf("record: %v\n", rec)
+
+	client, err := ethclient.Dial("http://localhost:8545")
+	if err != nil {
+		log.Fatalf("failed to connect to ethclient: %v", err)
+	}
+
+	account := common.HexToAddress("0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1")
+	log.Printf("Addr: %s", account.Hex())
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatalf("failed to get account balance: %v", err)
+	}
+
+	log.Println(balance)
 }
